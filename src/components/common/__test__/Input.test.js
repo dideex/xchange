b4439d3c65b054660752ee3fd77076217ab550de
DiDex <wc34fun@mail.ru>
2019-01-20 12:03:39 +0800

    Input validation tests

--- src/components/common/__test__/Input.test.js
+++ src/components/common/__test__/Input.test.js
@@ -10,6 +10,7 @@ jest.mock('coin-address-validator', () => ({
 describe('Input behaviour', () => {
   const testValue = 'Test value'
   const fakeMask = 'Btc'
+  const testPhone = '89148906030'
 
   it('markup', () => {
     const wrapper = shallow(<Component handleChange={() => {}} />)
@@ -59,6 +60,70 @@ describe('Input behaviour', () => {
         expect(hanldeErrorChange).toHaveBeenCalledTimes(1)
         expect(hanldeErrorChange).toHaveBeenCalledWith(false, expect.anything())
         expect(instance._validateWithMask(testValue, () => {})).toBe(testValue)
+        expect(instance.mask).toMatchSnapshot()
+      })
+
+      it('Validate with mask unvalidation currencies', () => {
+        const toggle = jest.fn()
+        const hanldeErrorChange = jest.fn()
+        WAValidator.validate = jest.fn(() => true)
+        const wrapper = mount(
+          <Component
+            handleErrorChange={hanldeErrorChange}
+            mask='LSK'
+            handleChange={toggle}
+            value="Not used value"
+          />,
+        )
+        const instance = wrapper.instance()
+        instance.handleChange({target: {value: testValue}})
+        expect(WAValidator.validate).toHaveBeenCalledTimes(0)
+        expect(hanldeErrorChange).toHaveBeenCalledTimes(0)
+        expect(instance._validateWithMask(testValue, () => {})).toBe(testValue)
+        expect(instance.mask).toMatchSnapshot()
+      })
+
+      it('Validate with mask phone', () => {
+        const toggle = jest.fn()
+        const hanldeErrorChange = jest.fn()
+        WAValidator.validate = jest.fn(() => true)
+        const wrapper = mount(
+          <Component
+            handleErrorChange={hanldeErrorChange}
+            mask='phone'
+            handleChange={toggle}
+            value="Not used value"
+          />,
+        )
+        const instance = wrapper.instance()
+        instance.handleChange({target: {value: testValue}})
+        expect(WAValidator.validate).toHaveBeenCalledTimes(0)
+        expect(hanldeErrorChange).toHaveBeenCalledTimes(1)
+        expect(hanldeErrorChange).toHaveBeenCalledWith(true, expect.anything())
+        expect(instance._validateWithMask(testValue, () => {})).toMatchSnapshot()
+        expect(instance.mask).toMatchSnapshot()
+      })
+
+      it('Validate phone with pattern', () => {
+        const toggle = jest.fn()
+        const hanldeErrorChange = jest.fn()
+        WAValidator.validate = jest.fn(() => true)
+        const wrapper = mount(
+          <Component
+            handleErrorChange={hanldeErrorChange}
+            mask='phone'
+            handleChange={toggle}
+            value="Not used value"
+            pattern="\d+"
+          />,
+        )
+        const instance = wrapper.instance()
+        instance.handleChange({target: {value: testPhone}})
+        expect(WAValidator.validate).toHaveBeenCalledTimes(0)
+        expect(hanldeErrorChange).toHaveBeenCalledTimes(1)
+        expect(hanldeErrorChange).toHaveBeenCalledWith(false, expect.anything())
+        expect(instance._validateWithMask(testPhone, () => {})).toMatchSnapshot()
+        expect(instance.mask).toMatchSnapshot()
       })
     })
   })
