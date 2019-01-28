ff92a76172586b84d82c4f12f131db18ff058349
DiDex <wc34fun@mail.ru>
2019-01-28 12:15:25 +0800

    Virtualized tests

--- src/components/common/__test__/Virtualized.test.js
+++ src/components/common/__test__/Virtualized.test.js
@@ -2,43 +2,73 @@ import React from 'react'
 import {Virtualized} from '../Virtualized'
 import {mount, shallow} from 'enzyme'
 import {StaticRouter} from 'react-router-dom'
-import {IntlProvider, intlShape, injectIntl} from 'react-intl'
-
-import messages from '../../../locale/en.json'
-const intlProvider = new IntlProvider({locale: 'en', messages}, {})
-const {intl} = intlProvider.getChildContext()
-
-function nodeWithIntlProp(node) {
-  return React.cloneElement(node, {intl})
-}
-
-export function shallowWithIntl(node, {context, ...additionalOptions} = {}) {
-  return shallow(nodeWithIntlProp(node), {
-    context: Object.assign({}, context, {intl}),
-    ...additionalOptions,
-  })
-}
-
-export function mountWithIntl(
-  node,
-  {context, childContextTypes, ...additionalOptions} = {},
-) {
-  return mount(nodeWithIntlProp(node), {
-    context: Object.assign({}, context, {intl}),
-    childContextTypes: Object.assign({}, {intl: intlShape}, childContextTypes),
-    ...additionalOptions,
-  })
-}
+import {mountWithIntl} from '../../../helpers/intl'
 
 describe('Virtualized tests', () => {
+  const intl = {formatMessage: ({id}) => id}
   it('Base markup', () => {
     const wrapper = mountWithIntl(
       <StaticRouter location="/" context={{}}>
-        <Virtualized intl={{}} parsedOrders={[]} endpoint="endpoint" />
+        <Virtualized intl={intl} parsedOrders={[]} endpoint="endpoint" />
       </StaticRouter>,
     )
+    expect(wrapper.html()).toMatchSnapshot()
+  })
 
-    console.log(wrapper.debug())
-    expect(1+1).toBe(2)
+  describe('Orders behaviour', () => {
+    it('Orderds markup', () => {
+      const orders = [
+        {
+          id: 'id',
+          created: 'created',
+          inputValue: 'inputValue',
+          outputValue: 'outputValue',
+          toWallet: 'toWallet',
+          paymentStatus: 'paymentStatus',
+        },
+      ]
+      const wrapper = mountWithIntl(
+        <StaticRouter location="/" context={{}}>
+          <Virtualized intl={intl} parsedOrders={orders} endpoint="endpoint" />
+        </StaticRouter>,
+      )
+      expect(wrapper.html()).toMatchSnapshot()
+    })
+
+    it('Orderds markup', () => {
+      const orders = [
+        {
+          id: 'id',
+          created: 'created',
+          inputValue: 'inputValue',
+          outputValue: 'outputValue',
+          toWallet: 'toWallet',
+          paymentStatus: 'paymentStatus',
+        },
+        {
+          id: 'id2',
+          created: 'created2',
+          inputValue: 'inputValue2',
+          outputValue: 'outputValue2',
+          toWallet: 'toWallet2',
+          paymentStatus: 'paymentStatus2',
+        },
+        {
+          id: 'id3',
+          created: 'created3',
+          inputValue: 'inputValue3',
+          outputValue: 'outputValue3',
+          toWallet: 'toWallet3',
+          paymentStatus: 'paymentStatus3',
+        },
+      ]
+      const wrapper = mountWithIntl(
+        <StaticRouter location="/" context={{}}>
+          <Virtualized intl={intl} parsedOrders={orders} endpoint="endpoint" />
+        </StaticRouter>,
+      )
+      expect(wrapper.html()).toMatchSnapshot()
+      console.log(wrapper.html())
+    })
   })
 })
---
+++ src/helpers/intl.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import {mount, shallow} from 'enzyme'
+import {IntlProvider, intlShape} from 'react-intl'
+
+import messages from '../locale/en.json'
+const intlProvider = new IntlProvider({locale: 'en', messages}, {})
+const {intl} = intlProvider.getChildContext()
+
+function nodeWithIntlProp(node) {
+  return React.cloneElement(node, {intl})
+}
+
+export function shallowWithIntl(node, {context, ...additionalOptions} = {}) {
+  return shallow(nodeWithIntlProp(node), {
+    context: Object.assign({}, context, {intl}),
+    ...additionalOptions,
+  })
+}
+
+export function mountWithIntl(
+  node,
+  {context, childContextTypes, ...additionalOptions} = {},
+) {
+  return mount(nodeWithIntlProp(node), {
+    context: Object.assign({}, context, {intl}),
+    childContextTypes: Object.assign({}, {intl: intlShape}, childContextTypes),
+    ...additionalOptions,
+  })
+}
