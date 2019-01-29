09fcc378f18b7a22b65a5553ba7c28cf070ef483
DiDex <wc34fun@mail.ru>
2019-01-29 12:19:47 +0800

    virtualizd tests

--- src/components/common/__test__/Virtualized.test.js
+++ src/components/common/__test__/Virtualized.test.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Virtualized} from '../Virtualized'
 import {mount, shallow} from 'enzyme'
 import {StaticRouter} from 'react-router-dom'
-import {mountWithIntl} from '../../../helpers/intl'
+import {mountWithIntl, shallowWithIntl} from '../../../helpers/intl'
 
 describe('Virtualized tests', () => {
   const intl = {formatMessage: ({id}) => id}
@@ -43,7 +43,7 @@ describe('Virtualized tests', () => {
           inputValue: 'inputValue',
           outputValue: 'outputValue',
           toWallet: 'toWallet',
-          paymentStatus: 'paymentStatus',
+          paymentStatus: 1,
         },
         {
           id: 'id2',
@@ -51,7 +51,7 @@ describe('Virtualized tests', () => {
           inputValue: 'inputValue2',
           outputValue: 'outputValue2',
           toWallet: 'toWallet2',
-          paymentStatus: 'paymentStatus2',
+          paymentStatus: 1,
         },
         {
           id: 'id3',
@@ -59,7 +59,7 @@ describe('Virtualized tests', () => {
           inputValue: 'inputValue3',
           outputValue: 'outputValue3',
           toWallet: 'toWallet3',
-          paymentStatus: 'paymentStatus3',
+          paymentStatus: 1,
         },
       ]
       const wrapper = mountWithIntl(
@@ -67,6 +67,15 @@ describe('Virtualized tests', () => {
           <Virtualized intl={intl} parsedOrders={orders} endpoint="endpoint" />
         </StaticRouter>,
       )
+      const table = wrapper.find('Table')
+      console.log(
+        table.propTypes.children(
+          {children: [<Column dataKey="foo" width={100} />]},
+          'children',
+          'Table',
+        ),
+      )
+
       expect(wrapper.html()).toMatchSnapshot()
       console.log(wrapper.html())
     })
