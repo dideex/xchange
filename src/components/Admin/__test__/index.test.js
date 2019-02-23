a498d779ef1a5695805d67f510cef3f37583cd58
DiDex <wc34fun@mail.ru>
2019-02-23 13:28:36 +0800

    Create react router dom  context

--- src/components/Admin/__test__/index.test.js
+++ src/components/Admin/__test__/index.test.js
@@ -1,13 +1,45 @@
 import React from 'react'
 import Component from '../index'
-import {shallow} from 'enzyme'
-import {Router as MemoryRouter} from 'react-router-dom'
+import {shallow, mount} from 'enzyme'
+import {MemoryRouter} from 'react-router-dom'
 import {createMemoryHistory} from 'history'
 
 import {MobxProvider} from '../../../helpers/mobx'
 import {fakeCurrnecy} from '../../../helpers'
 import CashStore from '../../../store/Cash'
 import UserStore from '../../../store/User'
+
+import {BrowserRouter} from 'react-router-dom'
+import {shape} from 'prop-types'
+
+// Instantiate router context
+const router = {
+  history: new BrowserRouter().history,
+  route: {
+    location: {
+      path: '/location',
+    },
+    match: {
+      path: '/location',
+      params: {
+        id: 123,
+      },
+    },
+  },
+}
+
+const createContext = () => ({
+  context: {router},
+  childContextTypes: {router: shape({})},
+})
+
+export function mountWrap(node) {
+  return mount(node, createContext())
+}
+
+export function shallowWrap(node) {
+  return shallow(node, createContext())
+}
 
 describe('Settings behaviour', () => {
   let cashStore
@@ -20,23 +52,18 @@ describe('Settings behaviour', () => {
   describe('Markup', () => {
     it('Basic markup', () => {
       const wrapper = shallow(
-        <MemoryRouter>
-          <MobxProvider>
+        <MobxProvider>
+          <MemoryRouter>
             <Component />
-          </MobxProvider>
-        </MemoryRouter>,
+          </MemoryRouter>
+        </MobxProvider>,
       )
       expect(wrapper.html()).toMatchSnapshot()
     })
 
     it.only('With order id', () => {
-      const history = createMemoryHistory({initialEntries: [route]})
-      const wrapper = shallow(
-        <MemoryRouter history={history}>
-          <MobxProvider>
-            <Component />
-          </MobxProvider>
-        </MemoryRouter>,
+      const wrapper = shallowWrap(
+        <Component cashStore={cashStore} userStore={userStore} />,
       )
       expect(wrapper.html()).toMatchSnapshot()
     })
