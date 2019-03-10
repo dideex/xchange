8cf09e3c9aa6f25bfab4dbacdb94a1b8fed4f342
DiDex <wc34fun@mail.ru>
2019-03-10 12:18:44 +0800

    Create router intl context helper

--- src/components/Admin/__test__/index.test.js
+++ src/components/Admin/__test__/index.test.js
@@ -1,22 +1,17 @@
 import React from 'react'
 import Component from '../index'
-import {shallow, mount} from 'enzyme'
+import {shallow} from 'enzyme'
 import {MemoryRouter} from 'react-router-dom'
 
 import {MobxProvider} from '../../../helpers/mobx'
 import CashStore from '../../../store/Cash'
 import UserStore from '../../../store/User'
 
-import {BrowserRouter} from 'react-router-dom'
-import {shape} from 'prop-types'
-
 import Api from '../../../components/Api'
 
 import {delay} from '../../../helpers'
 
-import {IntlProvider, intlShape} from 'react-intl'
-
-import messages from '../../../locale/en.json'
+import {mountWrap} from '../../../helpers/router-intl-context'
 
 const btc = {
   _id: 'test id',
@@ -42,55 +37,6 @@ jest.mock('../../../components/Api', () => ({
 jest.mock('../../../components/common/Noty.js', () => ({
   noty: () => {},
 }))
-
-// Instantiate router context
-const router = {
-  history: {...new BrowserRouter().history},
-  route: {
-    location: {
-      path: '/location',
-      href: '/location',
-    },
-    match: {
-      path: '/location',
-      params: {
-        id: 'test id',
-      },
-    },
-  },
-}
-
-const createContext = () => ({
-  context: {router},
-  childContextTypes: {router: shape({})},
-})
-
-const intlProvider = new IntlProvider({locale: 'en', messages}, {})
-const {intl} = intlProvider.getChildContext()
-
-const createMixedContext = ({
-  router,
-  context,
-  childContextTypes,
-  ...additionalOptions
-} = {}) => ({
-  context: Object.assign({}, {router}, context, {intl}),
-  childContextTypes: Object.assign(
-    {},
-    {router: shape({})},
-    {intl: intlShape},
-    childContextTypes,
-  ),
-  ...additionalOptions,
-})
-
-export function mountWrap(node) {
-  return mount(node, createMixedContext({router}))
-}
-
-export function shallowWrap(node) {
-  return shallow(node, createContext())
-}
 
 describe('Settings behaviour', () => {
   let cashStore
@@ -181,7 +127,7 @@ describe('Settings behaviour', () => {
         Api.get = () => Promise.resolve({data: btc})
         const emitSocket = jest.fn()
         const wrapper = mountWrap(
-          <MobxProvider cashStore={{...cashStore, emitSocket }} userStore={userStore}>
+          <MobxProvider cashStore={{...cashStore, emitSocket}} userStore={userStore}>
             <Component />
           </MobxProvider>,
         )
