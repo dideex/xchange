7860082718c80f9addc0bba0cb0bc93084151622
Тушин Николай <DiDex@MacBook-Pro-DiDex.local>
2019-04-01 20:23:13 +0800

    added auth tests

--- src/store/utils.js
+++ src/store/utils.js
@@ -2,15 +2,15 @@ import Cookie from 'js-cookie'
 
 export const setToken = (token, isAdmin) => {
   if (isAdmin) {
-    Cookie.set('token', token)
+    Cookie.set('key', token)
     Cookie.set('isAdmin', true)
-  } else Cookie.set('token', token)
+  } else Cookie.set('key', token)
 }
 
 export const logout = () => {
-  Cookie.remove('token')
+  Cookie.remove('key')
   Cookie.remove('isAdmin')
 }
 
-export const getToken = () => Cookie.get('token') || null
+export const getToken = () => Cookie.get('key') || null
 export const getAdminStatus = () => Cookie.get('isAdmin') || null
