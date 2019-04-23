34bd607c68e29215c72d3e718afec0f27f10b142
Тушин Николай <DiDex@MacBook-Pro-DiDex.local>
2019-04-23 11:56:14 +0800

    Payment proof tests

--- src/helpers/fixtures.js
+++ src/helpers/fixtures.js
@@ -1,7 +1,28 @@
 export const fakeCurrnecy = [
-  {order: 2, _id: 2, icon: 'bitcoin', id: 'bitcoin', name: 'lisk'},
-  {order: 1, _id: 1, icon: 'eth', id: 'eth', name: 'tether'},
-  {order: 3, _id: 3, icon: 'rur', id: 'rur', name: 'litecoin'},
+  {
+    order: 2,
+    _id: 2,
+    icon: 'bitcoin',
+    id: 'bitcoin',
+    name: 'lisk',
+    label: 'lisk label',
+  },
+  {
+    order: 1,
+    _id: 1,
+    icon: 'eth',
+    id: 'eth',
+    name: 'tether',
+    label: 'tether label',
+  },
+  {
+    order: 3,
+    _id: 3,
+    icon: 'rur',
+    id: 'rur',
+    name: 'litecoin',
+    label: 'litecoin label',
+  },
 ]
 
 export const fakeData = {
